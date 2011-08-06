0e5c0ef56a3fd256873d3ed984fcbbf0b43a2c91
Philip Guo <philip@pgbovine.net>
2011-08-05 18:01:51 -0700

    tiny refactoring

--- scripts/copy_dir_into_package.py
+++ scripts/copy_dir_into_package.py
@@ -1,9 +1,9 @@
-# Copy an entire directory (argv[1]) into (argv[2] + 'cde-root/')
+# Copy an entire directory argv[1] into (argv[2] + 'cde-root/')
 # ---
 #
 # Use okapi to copy over all sub-directories and symlinks, and to make
-# sure that all symlinks are properly munged to refer to relative paths
-# within the package.  (Note that rsync does NOT munge symlinks!)
+# sure that all symlinks are properly munged to point to relative paths
+# within the package.  (Note that rsync does NOT munge symlinks.)
 #
 # by Philip Guo
 
@@ -21,24 +21,24 @@ OKAPI_BIN = os.path.normpath(os.path.join(script_dir, "../okapi"))
 assert os.path.isfile(OKAPI_BIN)
 
 
-def copy_dir_into_package(basedir, package_root_dir):
+def copy_dir_into_package(basedir, dst_root_dir):
   assert os.path.isdir(basedir)
 
   for (d, subdirs, files) in os.walk(basedir):
     # first copy over the directory so that it exists even if it's empty:
-    run_cmd_print_stderr([OKAPI_BIN, d, '', package_root_dir])
+    run_cmd_print_stderr([OKAPI_BIN, d, '', dst_root_dir])
 
     # now copy over all the files
     for f in files:
       p = os.path.join(d, f)
-      run_cmd_print_stderr([OKAPI_BIN, p, '', package_root_dir])
+      run_cmd_print_stderr([OKAPI_BIN, p, '', dst_root_dir])
 
     # if any subdirs are symlinks, then copy them over as well to
     # preserve the original directory/symlink structure:
     for sd in subdirs:
       p = os.path.join(d, sd)
       if os.path.islink(p):
-        run_cmd_print_stderr([OKAPI_BIN, p, '', package_root_dir])
+        run_cmd_print_stderr([OKAPI_BIN, p, '', dst_root_dir])
 
 
 if __name__ == "__main__":
