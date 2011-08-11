db93f7e6cb52407c092fb3b6f767536beb093730
Philip Guo <philip@pgbovine.net>
2011-08-10 18:49:24 -0700

    minor update

--- tests/symlink_absolute_test/testme.py
+++ tests/symlink_absolute_test/testme.py
@@ -6,6 +6,6 @@ def checker_func():
   assert os.path.isfile(CDE_ROOT_DIR + '/home/pgbovine/CDE/tests/test_file.txt')
   assert os.path.islink(CDE_ROOT_DIR + '/home/pgbovine/CDE/tests/absolute-symlink.test_file.txt')
   assert os.readlink(CDE_ROOT_DIR + '/home/pgbovine/CDE/tests/absolute-symlink.test_file.txt') == \
-                     '../../../../home/pgbovine/CDE/tests/test_file.txt'
+                     './../../../..//home/pgbovine/CDE/tests/test_file.txt'
 
 generic_test_runner(["python", "symlink_absolute_test.py"], checker_func)
